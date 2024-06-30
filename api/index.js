bd0d9392fa38a511f0232fac8e817887de6e08b0
Pratham Shetty <shettypratham001@gmail.com>
2024-06-30 21:26:23 +0530

    Created SignUp Page functionality

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -11,6 +12,23 @@ mongoose.connect(process.env.MONGO).then(()=>{
 })
 
 const app=express();
+app.use(express.json());
+
 app.listen(3000,()=>{
-    console.log("Server is running on port 3000")
+    console.log("Server is running on port 3000");
+})
+
+app.get('/',(req,res)=>{
+    res.send("Hello World")
+})
+
+app.use('/api/auth',authRouter);
+app.use((err,req,res,next)=>{
+    const statusCode= err.statusCode || 500;
+    const message= err.message|| 'Internal Server Error'
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message,
+    })
 })
